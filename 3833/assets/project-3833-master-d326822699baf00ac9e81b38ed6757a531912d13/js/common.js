1ba8eb90572fc18a873bed284004a192519b98b8
Radha <git@genx-infotech.com>
2017-05-14 23:59:28 +0530

    projects

---
+++ 3833/assets/project-3833-master-d326822699baf00ac9e81b38ed6757a531912d13/js/common.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){		
+	// IE9 placeholder code
+	$('[placeholder]').each(function() {  
+		var input = $(this);
+					
+		$(input).focus(function(){
+			if (input.val() == input.attr('placeholder')) {
+			   input.val('').removeClass("placeholder");
+			}
+		});
+			
+		$(input).blur(function(){
+		   if (input.val() == '' || input.val() == input.attr('placeholder')) {
+			   input.val(input.attr('placeholder')).addClass("placeholder");
+		   }
+		});
+	}).blur();
+
+});
